c628b320ab5f222052b01236edb85660fd259a1f
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-02-11 00:52:04 +0200

    feat(admin): Adding users first_name last_name and email columns to admin dashboard user

--- custom_auth/admin.py
+++ custom_auth/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from custom_auth.models import CustomUser
 
-# Register your models here.
+
+class CustomModelAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "email"]
+
+
+admin.site.register(CustomUser, CustomModelAdmin)
