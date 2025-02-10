d86dd2cffef143ec1e9b5fde069c9f2b98d93ba6
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-02-11 01:57:32 +0200

    feat(static): Updating static files dirs and add signup template

--- ecom/settings.py
+++ ecom/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "ecom.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,11 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATIC_ROOT = [BASE_DIR, "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_ROOT = [BASE_DIR, "media"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
